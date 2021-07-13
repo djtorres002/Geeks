d81b8c6837033ffe14b04e54ac5f5cbffdb58871
Diego Torres <djtorres02@fb.com>
2021-07-12 19:24:41 -0500

    Creation of the Login activity

---
+++ app/src/main/java/com/example/geeks/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.geeks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+    }
+}
