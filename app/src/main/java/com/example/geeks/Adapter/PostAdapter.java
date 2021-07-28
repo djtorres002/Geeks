64779ebd1e7faa8ac5089c014a6d142dd91402bb
Diego Torres <djtorres02@fb.com>
2021-07-28 18:11:25 -0500

    PostAdapter implemented to allow the users see posts

--- app/src/main/java/com/example/geeks/Adapter/PostAdapter.java
+++ app/src/main/java/com/example/geeks/Adapter/PostAdapter.java
@@ -1,4 +1,105 @@
 package com.example.geeks.Adapter;
 
-public class PostAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.geeks.R;
+import com.example.geeks.models.Post;
+
+import org.parceler.Parcels;
+
+import java.util.Date;
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+    public static final String TAG = "PostAdapter";
+
+    List<Post> postList;
+    Context context;
+
+    public PostAdapter (List<Post> postList, Context context){
+        this.postList = postList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // This creates the view for our recycler view
+        View v = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+        // This sets the data in our View
+        Post post = postList.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        // This returns the number of posts in our list
+        return postList.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        postList.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> list) {
+        postList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        // Variables from our xml
+        TextView tvUser;
+        TextView tvDescription;
+        ImageView ivPicture;
+        TextView tvTime;
+        ImageView ivProfile;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvUser = itemView.findViewById(R.id.tvUsername);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+            ivPicture = itemView.findViewById(R.id.ivImage);
+            tvTime = itemView.findViewById(R.id.tvTime);
+            ivProfile = itemView.findViewById(R.id.ivProfile);
+        }
+
+        public void bind(Post post){
+            tvUser.setText(post.getUser().getUsername());
+            tvDescription.setText(post.getDescription());
+            Glide.with(context)
+                    .load(post.getUser().getParseFile("userPic").getUrl())
+                    .override(40, 40)
+                    .centerCrop()
+                    .into(ivProfile);
+
+            Glide.with(context)
+                    .load(post.getImage().getUrl())
+                    .override(410, 250)
+                    .centerCrop()
+                    .into(ivPicture);
+
+            Date createdAt = post.getCreatedAt();
+            String timeAgo = Post.calculateTimeAgo(createdAt);
+            tvTime.setText(timeAgo);
+        }
+    }
 }
--- app/src/main/java/com/example/geeks/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/geeks/Fragments/HomeFragment.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.geeks.Adapter.PostAdapter;
 import com.example.geeks.R;
 //import com.example.geeks.adapter.PostAdapter;
 import com.example.geeks.models.NewPostActivity;
@@ -33,8 +34,11 @@ public class HomeFragment extends Fragment {
 
     public static final String TAG = "HomeFragment";
     Context context;
-
     Button btNewPost;
+    List<Post> postList;
+    RecyclerView rvPost;
+    PostAdapter adapter;
+    private SwipeRefreshLayout swipeContainer;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -60,11 +64,53 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         btNewPost = view.findViewById(R.id.btNewPost);
+        rvPost = view.findViewById(R.id.rvPost);
+        postList = new ArrayList<>();
+        adapter = new PostAdapter(postList, context);
+        rvPost.setLayoutManager(new LinearLayoutManager(context));
+        rvPost.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
+        rvPost.setAdapter(adapter);
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                queryPosts();
+            }
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
+        queryPosts();
         btNewPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToNewPost();
+            }
+        });
+    }
+
+    private void queryPosts() {
+        // Create a query that gets the Post class
+        ParseQuery<Post> query = ParseQuery.getQuery("Post");
+        // Include the user data in our result
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATEDAT);
+        // Execute our query
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if(e == null){
+                    Log.d(TAG, "Post size: " + objects.size());
+                    postList.clear();
+                    postList.addAll(objects);
+                    adapter.notifyDataSetChanged();
+                    swipeContainer.setRefreshing(false);
+                }else{
+                    Log.d(TAG, "Error: " + e.getMessage());
+                }
             }
         });
     }
